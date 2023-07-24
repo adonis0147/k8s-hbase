f8a8bca97d383baae01eb84e5109bbc99853ea80
Adonis Ling <adonis0147@gmail.com>
2023-07-24 14:59:13 +0800

    Update the url of hadoop on aarch64 platform

--- packages/download_packages.sh
+++ packages/download_packages.sh
@@ -9,21 +9,29 @@ SCRIPT_PATH="$(
 readonly SCRIPT_PATH
 
 arch="$(uname -m)"
-# shellcheck disable=2034
-declare -r JAVA_URL="https://download.oracle.com/java/17/archive/jdk-17.0.8_linux-${arch/86_/}_bin.tar.gz"
+if [[ "${arch}" == 'arm64' ]]; then
+	arch='aarch64'
+fi
 
 if [[ "${arch}" == 'x86_64' ]]; then
 	# shellcheck disable=2034
+	declare -r JAVA_URL="https://download.oracle.com/java/17/archive/jdk-17.0.8_linux-${arch/86_/}_bin.tar.gz"
+	# shellcheck disable=2034
 	declare -r JAVA_SHA256SUM='74b528a33bb2dfa02b4d74a0d66c9aff52e4f52924ce23a62d7f9eb1a6744657'
+	# shellcheck disable=2034
+	declare -r HADOOP_URL='https://dlcdn.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6.tar.gz'
+	# shellcheck disable=2034
+	declare -r HADOOP_SHA256SUM='f5195059c0d4102adaa7fff17f7b2a85df906bcb6e19948716319f9978641a04'
 else
 	# shellcheck disable=2034
+	declare -r JAVA_URL="https://download.oracle.com/java/17/archive/jdk-17.0.8_linux-${arch}_bin.tar.gz"
+	# shellcheck disable=2034
 	declare -r JAVA_SHA256SUM=cd24d7b21ec0791c5a77dfe0d9d7836c5b1a8b4b75db7d33d253d07caa243117
+	# shellcheck disable=2034
+	declare -r HADOOP_URL="https://dlcdn.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6-${arch}.tar.gz"
+	# shellcheck disable=2034
+	declare -r HADOOP_SHA256SUM='e4bbf6b80ef604912f6b9bf6ca77323dee6028d08d38f36ca077df56de8a5d0d'
 fi
-
-# shellcheck disable=2034
-declare -r HADOOP_URL='https://dlcdn.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6.tar.gz'
-# shellcheck disable=2034
-declare -r HADOOP_SHA256SUM='f5195059c0d4102adaa7fff17f7b2a85df906bcb6e19948716319f9978641a04'
 
 function download() {
 	local package
