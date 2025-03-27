68465c1d59620123455c33779709d04ba28e0f68
Adonis Ling <adonis0147@gmail.com>
2025-03-27 19:21:12 +0800

    feat: use helm

--- build_images.sh
+++ build_images.sh
@@ -8,64 +8,17 @@ PROJECT_PATH="$(
 )"
 readonly PROJECT_PATH
 
-function download_packages() {
-	pushd "${PROJECT_PATH}/packages" &>/dev/null
-	bash download_packages.sh
-	popd &>/dev/null
-}
-
-function build_hdfs() {
-	pushd "${PROJECT_PATH}/hdfs" &>/dev/null
-
-	ln -f "${PROJECT_PATH}/packages"/jdk-*.tar.gz .
-	ln -f "${PROJECT_PATH}/packages"/hadoop-*.tar.gz .
-
-	docker build -t hdfs .
-
-	popd &>/dev/null
-
-	minikube image load --alsologtostderr hdfs:latest
-}
-
-function build_zookeeper() {
-	pushd "${PROJECT_PATH}/zookeeper" &>/dev/null
-
-	ln -f "${PROJECT_PATH}/packages"/jdk-*.tar.gz .
-	ln -f "${PROJECT_PATH}/packages"/apache-zookeeper-*.tar.gz .
-
-	docker build -t zookeeper .
-
-	popd &>/dev/null
-
-	minikube image load --alsologtostderr zookeeper:latest
-}
-
-function build_hbase() {
-	pushd "${PROJECT_PATH}/hbase" &>/dev/null
-
-	ln -f "${PROJECT_PATH}/packages"/jdk-*.tar.gz .
-	ln -f "${PROJECT_PATH}/packages"/hbase-*.tar.gz .
-
-	docker build -t hbase .
-
-	popd &>/dev/null
-
-	minikube image load --alsologtostderr hbase:latest
-}
-
 function main() {
-	download_packages
+	pushd "${PROJECT_PATH}"
 
-	if [[ "${#@}" == 0 ]]; then
-		build_hdfs
-		build_zookeeper
-		build_hbase
-	else
-		for package in "${@}"; do
-			local cmd="build_${package}"
-			"${cmd}"
-		done
-	fi
+	local dirs=(helm-hadoop helm-zookeeper helm-hbase)
+	local dir
+
+	for dir in "${dirs[@]}"; do
+		"${dir}/build-image.sh"
+	done
+
+	popd
 }
 
 main "${@}"
