b2287688819ce66ee33a2373044224a5179fe599
Adonis Ling <adonis0147@gmail.com>
2025-03-27 19:21:12 +0800

    feat: use helm

--- destroy_cluster.sh
+++ destroy_cluster.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-kubectl delete --ignore-not-found statefulsets.apps nn jn dn zk hm rs
-kubectl delete --ignore-not-found service namenode journalnode datanode zookeeper hmaster regionserver
+helm uninstall hbase
+helm uninstall zookeeper
+helm uninstall hadoop
+
 kubectl delete --ignore-not-found pvc \
 	namenode-data-nn-{0..1} journalnode-data-jn-{0..2} datanode-data-dn-{0..2} zookeeper-data-zk-{0..2}
-kubectl delete --ignore-not-found configmaps hdfs-conf zk-conf hbase-conf namenode-entrypoint zookeeper-entrypoint
--- start_cluster.sh
+++ start_cluster.sh
@@ -1,34 +1,20 @@
 #!/usr/bin/env bash
 
-kubectl create configmap hdfs-conf --from-file=hdfs/conf --save-config --dry-run=client -o yaml |
-	kubectl apply -f -
-kubectl create configmap zk-conf --from-file=zookeeper/conf --save-config --dry-run=client -o yaml |
-	kubectl apply -f -
-kubectl create configmap hbase-conf --from-file=hbase/conf --save-config --dry-run=client -o yaml |
-	kubectl apply -f -
+helm install --name-template=hadoop helm-hadoop
 
-kubectl apply -f hdfs/journalnode.yaml
 kubectl rollout status --watch --timeout=600s statefulset/jn
-
-kubectl apply -f hdfs/namenode.yaml
 kubectl rollout status --watch --timeout=600s statefulset/nn
-
-kubectl apply -f hdfs/datanode.yaml
 kubectl rollout status --watch --timeout=600s statefulset/dn
 
 sleep 30
 
-while ! kubectl exec -it nn-0 -- hdfs haadmin -transitionToActive nn0; do
-	sleep 10s
-done
+helm install --name-template=zookeeper helm-zookeeper
 
-kubectl apply -f zookeeper/zookeeper.yaml
 kubectl rollout status --watch --timeout=600s statefulset/zk
 
 sleep 30
 
-kubectl apply -f hbase/hmaster.yaml
-kubectl rollout status --watch --timeout=600s statefulset/hm
+helm install --name-template=hbase helm-hbase
 
-kubectl apply -f hbase/regionserver.yaml
+kubectl rollout status --watch --timeout=600s statefulset/hm
 kubectl rollout status --watch --timeout=600s statefulset/rs
